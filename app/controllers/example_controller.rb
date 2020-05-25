c4268abe55b13aba8f2d6b72914caa3ea5d17d3d
secondbaker <secondbaker@gmail.com>
2020-05-25 17:13:26 -0400

    I want to get the event example working, because then I would be able to read the events into a list.

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -4,7 +4,40 @@ class ExampleController < ApplicationController
   
       redirect_to client.authorization_uri.to_s
     end
-  
+
+    def callback
+        client = Signet::OAuth2::Client.new(client_options)
+        client.code = params[:code]
+    
+        response = client.fetch_access_token!
+    
+        session[:authorization] = response
+    
+        redirect_to calendars_url
+      end
+
+      def calendars
+        client = Signet::OAuth2::Client.new(client_options)
+        client.update!(session[:authorization])
+    
+        service = Google::Apis::CalendarV3::CalendarService.new
+        service.authorization = client
+    
+        @calendar_list = service.list_calendar_lists
+      end
+
+      def events
+        client = Signet::OAuth2::Client.new(client_options)
+        client.update!(session[:authorization])
+
+        service = Google::Apis::CalendarV3::CalendarService.new
+        service.authorization = client
+
+        calendars = service.list_calendar_lists
+        calendars.each do |calendar|
+            @event_list.push service.event_list(calendar.id)
+        end
+      end
     private
   
     def client_options
@@ -14,7 +47,8 @@ class ExampleController < ApplicationController
         authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
         token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
         scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
-        redirect_uri: "http://localhost:3000/callback"
+        redirect_uri: callback_url
       }
     end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
-  get '/redirect', to: 'example#redirect', as: :redirect
+  get '/redirect', to: 'example#redirect', as: 'redirect'
+  get '/callback', to: 'example#callback', as: 'callback'
+  get '/calendars', to: 'example#calendars', as: 'calendars'
+  get '/events', to: 'example#events', as: 'events'
 end
