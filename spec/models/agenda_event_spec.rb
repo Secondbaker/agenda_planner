23d5f34a5069abb685e788152945beae4d9ebdd3
secondbaker <secondbaker@gmail.com>
2020-05-25 14:45:39 -0400

    Got some tests up and running.  Everything seems to be working, and I can get tests to pass or fail if I want.

--- spec/models/agenda_event_spec.rb
+++ spec/models/agenda_event_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe AgendaEvent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.create(name: 'Event Name', start_time: Time.current - 1, end_time: Time.current) }
+  
+  it "is valid with valid attributes" do
+      puts subject.inspect
+      expect(subject).to be_valid
+  end
+  it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+  end
+  it "is not valid without a start_time" do
+      subject.start_time = nil
+      expect(subject).to_not be_valid
+  end
+  it "is not valid without an end_time" do
+      subject.end_time = nil
+      expect(subject).to_not be_valid
+  end
+  it "is not valid if end time is before start time" do
+      subject.end_time = subject.start_time + 1
+      expect(subject).to_not be_valid
+  end
 end
