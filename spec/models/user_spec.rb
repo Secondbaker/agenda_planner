299b42a29934030e8bf6c3262749aea968b0722a
secondbaker <secondbaker@gmail.com>
2020-05-29 11:10:54 -0400

    Got some basic tests on the User model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new }
+  context 'creating user' do
+    it 'with proper information succeeds' do
+      subject.email = Faker::Internet.email
+      subject.password = 'password'
+      subject.save
+      expect(User.all.size).to eq 1
+    end
+    it 'with no information fails' do
+      subject.save
+      expect(User.all.size).to eq 0
+    end
+    it 'without email fails' do
+      subject.password = 'password'
+      subject.save
+      expect(User.all.size).to eq 0
+    end
+  end
 end
