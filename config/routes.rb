f1b4e267c87c9173ba37ebc268fc5ff1d8c5bf45
secondbaker <secondbaker@gmail.com>
2020-05-29 10:49:16 -0400

    Added a Devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
   get '/redirect', to: 'example#redirect', as: 'redirect'
   get '/callback', to: 'example#callback', as: 'callback'
